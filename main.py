acebbcb4860ecb6f6843f3597ba0c58223103121
Evan Finger <105296253+EvanFinger@users.noreply.github.com>
2023-08-13 11:58:08 -0400

    8/13/2023 11:57 a
    Started first attempt at backwards propagation

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import keras
 import numpy as np
 from keras.datasets import mnist
 from matplotlib import pyplot
@@ -17,19 +16,28 @@ pyplot.subplot(330 + 1 + 1)
 pyplot.imshow(train_X[1], cmap=pyplot.get_cmap('gray'))
 pyplot.show()
 
-i0 = np.array(train_X[1])  # 784 inputs
-i0 = i0.flatten()
-for e in range(784):
-    i0[e] = i0[e] / 252
-h0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-h1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-o0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+def set_inout(x):
+    global i0
+    global trueout
+    i0 = np.array(train_X[x])  # 784 inputs
+    i0 = i0.flatten()
+    for e in range(784):
+        i0[e] = i0[e] / 252
+    trueout = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for element in range(10):
+        if element == train_y[x]:
+            trueout[train_y[x]] = 1
+
+
+z0 = [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
+s0 = [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
 w0 = np.random.uniform(-1, 1, (10, 784))  # first weight layer
 w1 = np.random.uniform(-1, 1, (10, 10))  # second weight layer
-w2 = np.random.uniform(-1, 1, (10, 10))  # third weight layer
-b0 = np.random.uniform(-1, 1, (10, 1))
-b1 = np.random.uniform(-1, 1, (10, 1))
-b2 = np.random.uniform(-1, 1, (10, 1))
+
+b0 = np.random.uniform(-1, 1, (1, 10))
+b1 = np.random.uniform(-1, 1, (1, 10))
+
 
 
 def sig(x):
@@ -43,24 +51,52 @@ def softmax(vec):
 
 
 def forward_propagation():
-    global o0
+    global s0
+    global i0
+    global s0_a
     for index in range(10):
-        h0[index] = np.sum(np.multiply(i0, w0[index]), 0)  # + b0[index]
-        h0[index] = sig(h0[index])
+        z0[index] = np.sum(np.multiply(i0, w0[index]), 0) + b0[0][index]
+        z0[index] = sig(z0[index])
     for index in range(10):
-        h1[index] = np.sum(np.multiply(h0, w1[index]), 0)  # + b1[index]
-        h1[index] = sig(h1[index])
+        s0[index] = np.sum(np.multiply(z0, w1[index]), 0) + b1[0][index]
+    s0_a = softmax(s0)
+
+
+def predict():
+    temp = 0
+    prediction = 0
     for index in range(10):
-        o0[index] = np.sum(np.multiply(h1, w2[index]), 0)
-    o0 = softmax(o0)
+        if s0[index] > temp:
+            temp = s0[index]
+            prediction = index
+    return prediction
+
+
+def cost():
+    global trueout
+    return np.square(np.subtract(trueout, s0))
+
+
+def back_propagation():
+    for w1_r in range(10):
+        for w1_c in range(10):
+            w1[w1_r][w1_c] = w1[w1_r][w1_c] - (2 * (s0_a[w1_r] - trueout[w1_r]))*(s0[w1_r])*(z0[w1_c])
+
+
+    print(s0_a)
+    print(trueout)
+    print(cost())
+    print(cost().mean())
+
 
 
 
 # for image in train_X:
-forward_propagation()
-print(h0)
-print(h1)
-print(o0)
+for index in range(1):
+    set_inout(index)
+    forward_propagation()
+    back_propagation()
+    print(predict())
 
 
 
